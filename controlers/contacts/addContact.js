d5e1106d6b93ddf863eb35ed77bb4dd23a05456f
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-07-19 21:56:35 +0300

    Update addContact.js

--- controlers/contacts/addContact.js
+++ controlers/contacts/addContact.js
@@ -4,7 +4,7 @@ const { contactAddSchema } = require("../../models");
 
 const addContact = async (req, res) => {
   const { error } = contactAddSchema.validate(req.body);
-  console.log(error);
+  // console.log(error);
   if (error) {
     throw createError(400, (message = "missing required name field"));
   }
