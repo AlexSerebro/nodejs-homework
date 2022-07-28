1d95b529b2228645088a300d122cc307f1aad353
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-07-28 21:57:41 +0300

    add model

--- models/user.js
+++ models/user.js
@@ -2,4 +2,38 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { version } = require("joi");
 
-const userSchema = Schema({}, { versionKey: false });
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false }
+);
+
+const User = model("user", userSchema);
+
+const joiSchema = Joi.object({
+  password: Joi.string.required(),
+  email: Joi.string.required(),
+});
+
+module.exports = {
+  User,
+  joiSchema,
+};
